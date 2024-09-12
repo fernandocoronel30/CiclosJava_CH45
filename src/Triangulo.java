fa8e4d31e6c077ea2f866cc738994a1953a48447
Fernando Coronel <fernando.coronel.dev@gmail.com>
2024-09-11 19:25:17 -0600

    Se terminan los proyectos de triangulo normal e invertido

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -1,10 +1,10 @@
 public class Triangulo {
 	public static void main(String[] args) {
 		for (int i = 0; i <= 6; i++) {
-			for (int j = 0; j <= 6; j++) {
+			for (int j = 0; j <= i; j++) {
 				System.out.print("*");
-			}
+			}//for j
 			System.out.println("");
-		}
+		}//for i
 	}//main
 }//class Triangulo
--- src/TrianguloReverse.java
+++ src/TrianguloReverse.java
@@ -1,4 +1,10 @@
-
 public class TrianguloReverse {
-
+	public static void main(String[] args) {
+		for (int i = 6; i > 0; i--) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}//for j
+			System.out.println("");
+		}//for i
+	}//main
 }
